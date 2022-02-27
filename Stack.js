2a5893329fcce04d1b58564922923086253a4fc5
naimsiddiquibd <naimsiddiquibd@gmail.com>
2022-02-27 19:35:48 +0600

    Stact updated

--- Stack.js
+++ Stack.js
@@ -5,13 +5,27 @@ class Stack {
     add(element) {
         this.stack.push(element)
     }
+    remove() {
+        this.stack.pop()
+    }
 }
 
 const plates = new Stack();
 plates.add('A');
+console.log(plates.stack);
 plates.add('B');
+console.log(plates.stack);
 plates.add('C');
+console.log(plates.stack);
 plates.add('D');
+console.log(plates.stack);
 plates.add('E');
-
+console.log(plates.stack);
+plates.remove();
+console.log(plates.stack);
+plates.remove();
+console.log(plates.stack);
+plates.remove();
+console.log(plates.stack);
+plates.remove();
 console.log(plates.stack);
