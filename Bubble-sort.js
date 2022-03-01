6d1115c38460bee38e005119528f2cbc654867d1
naimsiddiquibd <naimsiddiquibd@gmail.com>
2022-03-01 18:36:56 +0600

    Bubble sort updated

--- Bubble-sort.js
+++ Bubble-sort.js
@@ -13,18 +13,5 @@ function bubbleSort(array){
     return array;
 }
 
-function bubbleSortDesc(array){
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if(array[j] < array[j+1]){
-                const temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-    return array;
-}
-
 const sorted = bubbleSort(numbers);
 console.log(sorted);
